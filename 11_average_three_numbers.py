53842c984f37fc093e76112d8a1b17d8c540f4f1
kalsoomk <khankalsoom371@gmail.com>
2025-09-04 23:47:06 +0500

    Add files via upload
    This program asks the user for three numbers, calculates their average, and prints it using an f-string. It demonstrates input handling, type conversion, arithmetic operations, and basic Python syntax.

---
+++ 11_average_three_numbers.py
@@ -0,0 +1,8 @@
+# Average of three numbers
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+num_3 = int(input("Enter third number: "))
+
+average = (num_1 + num_2 + num_3) / 3
+print(f"The average of all numbers is: {average}")
