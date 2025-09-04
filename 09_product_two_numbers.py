66ea3f95a4c054bd71c74e77a16913530dc61359
kalsoomk <khankalsoom371@gmail.com>
2025-09-04 22:23:58 +0500

    Add files via upload
    This program asks the user for two numbers, converts them to integers, and prints their product using an f-string. It demonstrates input handling, type conversion, and basic arithmetic.

---
+++ 09_product_two_numbers.py
@@ -0,0 +1,6 @@
+# Product of two numbers
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+product = num_1 * num_2
+print(f"The product of both numbers is: {product}")
