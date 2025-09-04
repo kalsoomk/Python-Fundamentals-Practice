a3c2c8aa3d10e7014322b8d682129b2cd0a54460
kalsoomk <khankalsoom371@gmail.com>
2025-09-05 00:02:34 +0500

    Add files via upload
    This program asks the user for a temperature in Celsius, converts it to Fahrenheit using the formula F = (C * 9/5) + 32, and prints the result. It demonstrates input handling, arithmetic operations, and f-string usage in Python.

---
+++ 12_celsius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+# Celsius to Fahrenheit Converter
+
+temperature_c = int(input("Enter temperature in Celsius: "))
+temperature_f = (temperature_c * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {temperature_f}")
