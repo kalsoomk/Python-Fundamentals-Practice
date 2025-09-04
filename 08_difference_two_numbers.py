ba2795aed93ed0a1b4f3a4abd25e6018093ca998
kalsoomk <khankalsoom371@gmail.com>
2025-09-04 22:19:47 +0500

    Add files via upload
    This program asks the user for two numbers, converts them to integers, and prints the difference (first minus second) using an f-string. It demonstrates input handling, type conversion, and basic arithmetic.

---
+++ 08_difference_two_numbers.py
@@ -0,0 +1,6 @@
+# Difference of two numbers
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+difference = num_1 - num_2
+print(f"The difference of both numbers is: {difference}")
