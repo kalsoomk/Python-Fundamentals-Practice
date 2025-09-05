ed746e8f6383464f701a2fcdd5610346fd0e6f6f
kalsoomk <khankalsoom371@gmail.com>
2025-09-05 20:07:53 +0500

    Add files via upload
    This program asks the user for a distance in meters, converts it to kilometers, and prints the result. Demonstrates input handling, arithmetic, and f-string formatting.

---
+++ 28_meters_to_km.py
@@ -0,0 +1,5 @@
+# Convert Meters to Kilometers
+
+distance_in_meters = int(input("Enter the distance in meters: "))
+kilometers = distance_in_meters / 1000
+print(f"Distance in kilometers is: {kilometers}")
